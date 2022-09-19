d52b8e0210650409b6ea7537695bc914d8ca77f4
Pedrow <pedropberger@gmail.com>
2022-09-19 19:32:53 -0300

    connection code

---
+++ Scripts/db_con_start.py
@@ -0,0 +1,46 @@
+"""Connection to DB"""
+
+from distutils.command.config import config
+from typing_extensions import Self
+from numpy import extract
+import py
+import requests
+import pandas as pd
+import os
+import sqlite3
+import datetime
+import time
+
+initialdate = []
+finaldate = []
+portaltp_executivo=[]
+metlist = []
+local_apilist = []
+local_db = []
+exec(open("scripts/API_organization.py").read())
+exec(open("config.py").read())
+
+def dbclean():
+    """Cleaning old DB"""
+
+    os.remove("C:\TempData\Julius.db") if os.path.exists("C:\TempData\Julius.db") else None
+    return print('Database Cleaned!')
+
+def dbstart():
+    """Start connection and cursor setup"""
+
+    conn = sqlite3.connect('C:\TempData\Julius.db')
+    type(conn)
+    cur = conn.cursor()
+    type(cur)
+    print('Conection to Database Created')
+    return conn
+
+def dbclose(conn):
+    """Close connection"""
+
+    conn.close()
+
+    return print('Database Closed')
+
+conn = dbstart()
---
+++ main.py
@@ -0,0 +1,2 @@
+"""This file run all files in order"""
+
