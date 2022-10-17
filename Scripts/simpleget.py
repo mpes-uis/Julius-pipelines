e5a2d90c6b6586a1673b4e21493d8b51ceb3f4e5
Pedrow <pedropberger@gmail.com>
2022-10-17 16:38:30 -0300

    tests for Airflow

---
+++ Scripts/simpleget.py
@@ -0,0 +1,8 @@
+import requests
+import pandas as pd
+
+res = requests.get('https://santamariadejetiba-es.portaltp.com.br/api/transparencia.asmx/json_licitacoes?ano=2018&mes=1')
+
+dflicitacoes = pd.DataFrame(pd.read_json(res.text.replace('<?xml version="1.0" encoding="utf-8"?>', '').\
+                                         replace('<string xmlns="http://tempuri.org/">', '').removesuffix('</string>')))
+print(dflicitacoes)
